40901b5a31e0f0a310bf0b61154e7d5bad53a123
Gabe Bravo <gabecbravo@gmail.com>
2017-09-30 11:46:14 -0400

    updating

---
+++ client/src/login/login_actions.js
@@ -0,0 +1,10 @@
+// ACTIONS & CREATORS FOR LOGIN FORM
+export const SET_LOGIN_FORM = 'SET_LOGIN_FORM';
+export const setLoginForm = () => ({
+
+});
+
+export const RESET_LOGIN_FORM = 'RESET_LOGIN_FORM';
+export const resetLoginForm = () => ({
+  
+})
