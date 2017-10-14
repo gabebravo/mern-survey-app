860221bf084d95fe39504ada0b07e77da4bcbd71
Gabe Bravo <gabecbravo@gmail.com>
2017-10-13 23:07:51 -0400

    deploying.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const server = require('./lib/server');
 const db     = require('./lib/db');
 
+console.log(process.env.PORT)
+
 db.connect((err) => {
   if (err) {
     return console.log(`DB is not working...`);
