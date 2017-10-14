ef2c193980cbed67250b65a40a1577f9f6830fd1
Gabe Bravo <gabecbravo@gmail.com>
2017-10-13 23:44:24 -0400

    changes

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 require('dotenv').config();
+// import dependencies
+const express = require('express');
+// const morgan     = require('morgan');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {PORT} = require('../config');
 
-const server = require('./lib/server');
-const db     = require('./lib/db');
+// mongose setup & config
+const mongoose = require('mongoose');
+const db = process.env.DATABASE_URL;
 
-db.connect({
-  useMongoClient: true,
-  /* other options */
+mongoose.Promise = global.Promise;
+mongoose.connect(db, {
+  useMongoClient: true, // weird new flag mongoose requires
 });
+
+// import express and instantiate a server
+const app = express();
+const port = process.env.PORT;
+
+// import and use body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// this is will add the client in deployment
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+const userRouter = require('../routes/user');
+app.use('/user', userRouter);
+
+const surveyRouter = require('../routes/survey');
+app.use('/survey', surveyRouter);
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+module.exports = {app};
